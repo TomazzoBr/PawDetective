034d0f4b5538d6ea5a821c708b6194d8539ee846
gregae <grega.erzen@outlook.com>
2021-11-20 12:35:36 +0100

    fix: re-render list on filter btn change

--- paw-detective/client/src/components/Dashboard/PawsList/PawList.js
+++ paw-detective/client/src/components/Dashboard/PawsList/PawList.js
@@ -10,24 +10,24 @@ const PawsList = () => {
   const {customProps} = useContext(globalContext);
   const {paws, filterBtn} = customProps
 
-  const pawsList = paws
-      .filter(paw => function(paw) {
-        console.log('from pawlist',filterBtn)
-        if (filterBtn === "All") {
-          return paw;
-        } else if (filterBtn === "Lost") {
-          return paw.lostOrFound === true;
-        } else if (filterBtn === "Found") {
-          return paw.lostOrFound === false;
-        }
-      })
-      .map((paw) => (<PawsItem paw={paw} key={paw.id}/>));
-
+  const renderPaws = (paws) => {
+    console.log(paws)
+      return paws.filter(paw => {
+          if (filterBtn === "Lost") {
+            return paw.lostOrFound === true;
+          } else if (filterBtn === "Found") {
+            return paw.lostOrFound === false;
+          }
+          return paws
+        })
+        .map((paw) => (<PawsItem paw={paw} key={paw.id}/>));
+  }
 
   return (
     <ul className="list-container">
       <h4 className="click-add-h4">Click a pet to see more details</h4>
-      {pawsList.length ? pawsList : <p>there no pets in this list😉</p>}
+      {renderPaws(paws)}
+      {/* {pawsList.length ? pawsList : <p>there no pets in this list😉</p>} */}
     </ul>
   );
 };
